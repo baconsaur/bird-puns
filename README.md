6ec280eff625a25d6dec6967c9897cf5464f9d59
Ash Kestrel <tetrapteryxgames@gmail.com>
2016-02-17 20:15:39 -0700

    :rooster: Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+#Bird Pun Generator
+Need some stupid bird puns? Of course you do.
+
+##How to Install
+
+``bash
+npm install bird-puns
+``
+##How to Use
+
+``js
+var birdPuns = require('bird-puns');
+birdPuns.getBirdPun(); //If you liked it then you should've put a wing on it
+``
---
+++ app.js
@@ -0,0 +1,18 @@
+module.exports = {
+	getBirdPun: function() {
+		var puns = [
+			'Meet single birds in your area!',
+			'Well this is hawkward…',
+			'Owl drink to that',
+			'Toucan play at this game',
+			'I suspect fowl play',
+			'Stay coo',
+			'Where do birds get drunk? A crowbar',
+			'I just caw it like I see it',
+			'Tweet yo self!',
+			'Have a pheasant evening',
+			'if you liked it then you should\'ve put a wing on it'
+		];
+		return puns[Math.floor(Math.random() * puns.length)];
+	}
+};
